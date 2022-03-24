a678ff47cd7be181d7b85933569550e78accb5c6
Norihiro Seto <norihiro.seto@protonmail.com>
2022-03-24 16:17:37 +0900

    Add skaffold
    fix script
    
    
    fix
    
    
    Add k3d
    
    
    Add kustomize
    
    
    Add istioctl
    
    
    Fix kustomize
    
    
    Add krew
    
    
    Add include
    
    
    Add gomplate
    
    
    Add aws cli
    
    
    Add gRPC
    
    
    Merge K8s tools
    
    
    grpc

--- install.sh
+++ install.sh
@@ -10,56 +10,161 @@ set -a
 . ./devcontainer-features.env
 set +a
 
+architecture="$(uname -m)"
+case ${architecture} in
+    x86_64) architecture="amd64";;
+    aarch64 | armv8*) architecture="arm64";;
+    # aarch32 | armv7* | armvhf*) architecture="arm";;
+    # i?86) architecture="386";;
+    *) echo "(!) Architecture ${architecture} unsupported"; exit 1 ;;
+esac
 
-if [ ! -z ${_BUILD_ARG_HELLOWORLD} ]; then
-    echo "Activating feature 'helloworld'"
+os="$(uname -s)"
+case ${os} in
+    Linux) os="linux";;
+    # Darwin) os="darwin";;
+    *) echo "(!) OS ${os} unsupported"; exit 1 ;;
+esac
 
-    # Build args are exposed to this entire feature set following the pattern:  _BUILD_ARG_<FEATURE ID>_<OPTION NAME>
-    GREETING=${_BUILD_ARG_HELLOWORLD_GREETING:-undefined}
+if [ ! -z ${_BUILD_ARG_KUBERNETES} ]; then
+    # Skaffold
+    if [ "xnone" != "x${_BUILD_ARG_KUBERNETES_SKAFFOLD}" ]; then
+        echo "Activating feature 'skaffold'"
 
-    tee /usr/hello.sh > /dev/null \
-    << EOF
-    #!/bin/bash
-    RED='\033[0;91m'
-    NC='\033[0m' # No Color
-    echo -e "\${RED}${GREETING}, \$(whoami)!"
-    echo -e "\${NC}"
+        VERSION=${_BUILD_ARG_KUBERNETES_SKAFFOLD:-latest}
+
+        curl -sLo /tmp/skaffold https://storage.googleapis.com/skaffold/releases/${VERSION}/skaffold-${os}-${architecture}
+        install /tmp/skaffold /usr/local/bin/ && rm -f /tmp/skaffold
+    fi
+
+    # K3d
+    if [ "xnone" != "x${_BUILD_ARG_KUBERNETES_K3D}" ]; then
+        echo "Activating feature 'k3d'"
+
+        VERSION=${_BUILD_ARG_KUBERNETES_K3D:-latest}
+
+        curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=${K3D_VERSION} bash
+    fi
+
+    # Kustomize
+    if [ "xnone" != "x${_BUILD_ARG_KUBERNETES_KUSTOMIZE}" ]; then
+        echo "Activating feature 'kustomize'"
+
+        # Build args are exposed to this entire feature set following the pattern:  _BUILD_ARG_<FEATURE ID>_<OPTION NAME>
+        VERSION=${_BUILD_ARG_KUBERNETES_KUSTOMIZE:-latest}
+        if [ ${VERSION} = "latest" ] ; then
+            VERSION=""
+        fi
+
+        curl -sLo /tmp/install_kustomize.sh "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"
+        chmod +x /tmp/install_kustomize.sh
+
+        (
+            cd /tmp &&
+            ./install_kustomize.sh ${VERSION} &&
+            install kustomize /usr/local/bin/ && rm -f kustomize /go/kustomize /tmp/install_kustomize.sh
+        )
+    fi
+
+    # Istio CLI
+    if [ "xnone" != "x${_BUILD_ARG_KUBERNETES_ISTIOCTL}" ]; then
+        echo "Activating feature 'istioctl'"
+
+        # Build args are exposed to this entire feature set following the pattern:  _BUILD_ARG_<FEATURE ID>_<OPTION NAME>
+        VERSION=${_BUILD_ARG_KUBERNETES_ISTIOCTL:-latest}
+        if [ ${VERSION} = "latest" ] ; then
+            VERSION=""
+        fi
+
+        (
+            mkdir /tmp/istio &&
+            cd /tmp/istio && curl -L https://istio.io/downloadIstio | ISTIO_VERSION=${VERSION} sh - &&
+            install /tmp/istio/istio-*/bin/istioctl /usr/local/bin &&
+            rm -rf /tmp/istio
+        )
+    fi
+
+    # Krew Installer
+    if [ "xnone" != "x${_BUILD_ARG_KUBERNETES_KREW}" ]; then
+        echo "Activating feature 'krew'"
+
+        mkdir -p /usr/local/install/k8s
+        cat <<EOF > /usr/local/install/k8s/krew.sh
+#!/usr/bin/env bash
+set -e
+#--------------------------------------
+# Krew
+#--------------------------------------
+(
+  set -x; cd "\$(mktemp -d)" &&
+  OS="\$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="\$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-\${OS}_\${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/\${KREW}.tar.gz" &&
+  tar zxvf "\${KREW}.tar.gz" &&
+  ./"\${KREW}" install krew
+)
+echo 'export PATH="\${KREW_ROOT:-\$HOME/.krew}/bin:\$PATH"' >> \$HOME/.bashrc
+PATH="\${HOME}/.krew/bin:\$PATH" kubectl krew install ns
+PATH="\${HOME}/.krew/bin:\$PATH" kubectl krew install ctx
 EOF
+        chmod +x /usr/local/install/k8s/krew.sh
 
-    chmod +x /usr/hello.sh
-    sudo cat '/usr/hello.sh' > /usr/local/bin/hello
-    sudo chmod +x /usr/local/bin/hello
+    fi
 fi
 
+# Gomplate
+if [ ! -z ${_BUILD_ARG_GOMPLATE} ]; then
+    echo "Activating feature 'gomplate'"
 
-if [ ! -z ${_BUILD_ARG_COLOR} ]; then
-    echo "Activating feature 'color'"
+    VERSION=${_BUILD_ARG_GOMPLATE_VERSION:-latest}
 
-    # Build args are exposed to this entire feature set following the pattern:  _BUILD_ARG_<FEATURE ID>_<OPTION NAME>
+    curl -sLo gomplate https://github.com/hairyhenderson/gomplate/releases/download/${VERSION}/gomplate_${os}-${architecture}-slim
+    install gomplate /usr/local/bin
+    rm gomplate
+fi
 
-    if [ "${_BUILD_ARG_COLOR_FAVORITE}" == "red" ]; then
-        FAVORITE='\\033[0\;91m'
+# Python pip3
+if [ ! -z ${_BUILD_ARG_PYTHON-PIP3} ]; then
+    echo "Activating feature 'python-pip3'"
+    apt-get update
+    apt-get -y install --no-install-recommends python3-pip
+    apt-get autoremove -y
+    apt-get clean -y
+    rm -rf /var/lib/apt/lists/*
+fi
+
+# AWS CLI
+if [ ! -z ${_BUILD_ARG_AWSCLI} ]; then
+    echo "Activating feature 'awscli'"
+
+    pip3 install awscli
+fi
+
+# Protocolbuffer Compiler
+if [ ! -z ${_BUILD_ARG_PROTOC} ]; then
+    echo "Activating feature 'protobuf-compiler'"
+
+    apt-get update
+    apt-get -y install --no-install-recommends protobuf-compiler
+
+    if [ "xtrue" = "x${_BUILD_ARG_PROTOC_GOLANG}" ]; then
+        echo "Installing golang gRPC..."
+
+        go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.26
+        go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1
+        go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v2.5.0
+        go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@v2.5.0
     fi
 
-    if [ "${_BUILD_ARG_COLOR_FAVORITE}" == "green" ]; then
-        FAVORITE='\\033[0\;32m'
+    if [ "xtrue" = "x${_BUILD_ARG_PROTOC_PYTHON}" ]; then
+        echo "Installing python gRPC..."
+
+        python3 -m pip install grpcio
+        python3 -m pip install grpcio-tools
     fi
 
-    if [ "${_BUILD_ARG_COLOR_FAVORITE}" == "gold" ]; then
-        FAVORITE='\\033[0\;33m'
-    fi
-
-    tee /usr/color.sh > /dev/null \
-    << EOF
-    #!/bin/bash
-    NC='\033[0m' # No Color
-
-    FAVORITE=${FAVORITE}
-    echo -e "\${FAVORITE} This is my favorite color! \${NC}"
-EOF
-
-    chmod +x /usr/color.sh
-    sudo cat '/usr/color.sh' > /usr/local/bin/color
-    sudo chmod +x /usr/local/bin/color
-
+    apt-get autoremove -y
+    apt-get clean -y
+    rm -rf /var/lib/apt/lists/*
 fi
